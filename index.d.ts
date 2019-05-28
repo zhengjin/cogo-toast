1b0e37139209633005e1e0e7a1a0eabebf7a6f5a
anmolmahatpurkar <anmolmahatpurkar@gmail.com>
2019-05-28 15:08:57 +0530

    Fix for top-level typings declaration

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,37 @@
-declare module 'cogo-toast' {
-	import { ReactNode, MouseEventHandler } from 'react';
+import { ReactNode, MouseEventHandler } from 'react';
 
-	export type Options = Partial<{
-		hideAfter: number;
-		position:
-			| 'top-left'
-			| 'top-center'
-			| 'top-right'
-			| 'bottom-left'
-			| 'bottom-center'
-			| 'bottom-right';
-		heading: string;
-		icon: ReactNode;
-		bar: Partial<{
-			size: string;
-			style: 'solid' | 'dashed' | 'dotted';
-			color: string;
-		}>;
-		onClick: MouseEventHandler;
+export type Options = Partial<{
+	hideAfter: number;
+	position:
+		| 'top-left'
+		| 'top-center'
+		| 'top-right'
+		| 'bottom-left'
+		| 'bottom-center'
+		| 'bottom-right';
+	heading: string;
+	icon: ReactNode;
+	bar: Partial<{
+		size: string;
+		style: 'solid' | 'dashed' | 'dotted';
+		color: string;
 	}>;
+	onClick: MouseEventHandler;
+}>;
 
-	export type HideToastFunction = () => void;
+export type HideToastFunction = () => void;
 
-	export type Method = {
-		(message: string, options?: Options & { hideAfter: 0 }): HideToastFunction;
-		(message: string, options?: Options): Promise<void>;
-	};
+export type Method = {
+	(message: string, options?: Options & { hideAfter: 0 }): HideToastFunction;
+	(message: string, options?: Options): Promise<void>;
+};
 
-	namespace toast {
-		export const loading: Method;
-		export const success: Method;
-		export const error: Method;
-		export const warn: Method;
-		export const info: Method;
-	}
-
-	export default toast;
+declare namespace cogoToast {
+	export const loading: Method;
+	export const success: Method;
+	export const error: Method;
+	export const warn: Method;
+	export const info: Method;
 }
+
+export default cogoToast;
